e528ee6791cee762c0253eef67d35a4bea7dba5a
Delta <editorrus2@gmail.com>
2021-12-18 14:12:58 +0500

    ffs PyCharm, **stop** optimizing imports on init.py

--- av_clipboard_lib/__init__.py
+++ av_clipboard_lib/__init__.py
@@ -1,2 +1,12 @@
+from av_clipboard_lib import clipboard_data
+from av_clipboard_lib import av_objects
+from av_clipboard_lib import base85
+from av_clipboard_lib import varint
 
-
+from av_clipboard_lib.clipboard_data import parse_av_clipboard_data, produce_av_clipboard_data
+from av_clipboard_lib.av_objects import (
+    Tap, Hold, Mine, Roll, Lift, Fake,
+    BPM, Stop, Delay, Warp, TimeSignature, Ticks, Combo,
+    Speed, Scroll, FakeSegment, Label
+)
+from av_clipboard_lib.base_types import RowPosition, TimePosition
